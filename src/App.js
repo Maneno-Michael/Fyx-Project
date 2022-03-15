2b077c5310145c46d153044ac4e2175e0dc5bc9f
Bahatimyk <manenomichael99@gmail.com>
2022-03-15 15:58:02 +0300

    Project innitiation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Profile from './Pages/Profile/Profile';
+import About from './Pages/About/About';
+import BookService from './Pages/BookService/BookService';
+import Feedback from './Pages/Feedback/Feedback';
+import Policy from './Pages/Policy/Policy';
+import History from './Pages/History/History';
+import Order from './Pages/Order/Order';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+         <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/register" element={<Register />}></Route>
+            <Route exact path="/profile" element={<Profile />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/bookservice" element={<BookService />}></Route>
+            <Route exact path="/feedback" element={<Feedback />}></Route>
+            <Route exact path="/history" element={<History />}></Route>
+            <Route exact path="/order" element={<Order />}></Route>
+            <Route exact path="/policy" element={<Policy />}></Route>
+            <Route exact path="/" element={<Home />}></Route>
+        </Routes>   
+        </Router>
   );
 }
 
