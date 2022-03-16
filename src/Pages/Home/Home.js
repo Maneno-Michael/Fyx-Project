f4e7a7c58c5b7e3ef4442f3e965395cbe7636ee9
sir yussuf <godanahassan61@gmail.com>
2022-03-16 10:12:39 +0300

    body

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import {Col,Row,Container} from 'react-bootstrap';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import "./Home.css";
 function Home() {
     return ( 
         <div>
-            
-        </div>
+        <section >
+        <Container>
+          <Row>
+            <Col>Hello</Col>
+            <Col>Hello2</Col>
+          </Row>
+        </Container>
+</section>
+</div>
      );
 }
 
