988e24864529bb8e49002db72efe08881b6adbf3
sir yussuf <godanahassan61@gmail.com>
2022-03-28 17:04:28 +0300

    tech

--- src/Technician/Wallet/Wallet.js
+++ src/Technician/Wallet/Wallet.js
@@ -40,9 +40,9 @@ ChartJS.register(
 
 
 function Wallet() {
-  const [show, setShow] = useState(false);
+  // const [show, setShow] = useState(false);
 
-  const submitModalHandler = () => setShow(false);
+  // const submitModalHandler = () => setShow(false);
 
 
 
@@ -97,9 +97,12 @@ function Wallet() {
                 <h5 className = "ml-5 font-weight-bold">Available Cash</h5>
                 <p className = "ml-5 mb-0 availableCash"style={{textDecoration:"underline"}}><span className="mr-2">Ksh.</span>5,000.00</p>
                 <p className="text-right  mb-0"> </p>
-                <button  onClick={() => setShow(true)}  style={{width:"20%",marginLeft:"83%",}}>Withdraw</button>
+               
                 
-                
+                <div className="squid" >
+                   <button className="bt btn-primary" type="button"data-bs-target="#myModal"data-bs-toggle="modal">open first modal</button>
+                         
+                </div>
         {/* modal event */}
                 
    
@@ -111,11 +114,24 @@ function Wallet() {
             </div>
             </div>
             </div>
+
+                      <div className="modal">
+                        <h3>hello</h3>
+                      </div>
+                      <div className="modal-body">
+
+                      </div>
+                      <div className="modal-footer">
+
+                      </div>
+
+
+
 {/* modal */}
-            <Modal show={show} submit={submitModalHandler}
+            {/* <Modal show={show} submit={submitModalHandler}
              />
              { show ? <div onClick={submitModalHandler} className="back-drop"></div> : null }
-   
+    */}
                  <div className="chart-holder">
                  <h3 className="secondary-heading mb-4">
                   Summary Earnings
