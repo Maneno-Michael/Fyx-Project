64ed59bb3f1e1db45ad3fb2905e46a704747eaca
lilvatty <lilvattykid@gmail.com>
2022-04-12 15:47:47 +0300

    google

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -220,6 +220,18 @@ function Homepage() {
     <div className="technicians">
         <h2>Technicians spreadout in kenya</h2>
         <div className="map">
+
+        <iframe
+         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127641.1704236439!2d36.77735686034806!3d-1.3030363544163734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi!5e0!3m2!1sen!2ske!4v1649766092949!5m2!1sen!2ske"
+        width="100%"
+        height="100%"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+      ></iframe>
+
+
+      
           
 
 
